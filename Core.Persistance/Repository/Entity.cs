ec27b28c570ccb82c24b15122ec8b0015d788759
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-15 04:18:41 +0300

    Add project files.

---
+++ Core.Persistance/Repository/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Repository;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+    public Entity()
+    {
+        Id = default;
+    }
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
---
+++ Core.Persistance/Repository/IRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Repository;
+
+public interface IRepository<TEntity, TEntityId>: IQueryable<TEntity>
+    where TEntity : Entity<TEntityId>
+{
+    TEntity? Get(
+        Expression<Func<TEntity, bool>> predicate,
+        //join atabilmemizi sağlayacak
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        // veri tabanından silinenleri de görmek için true olmalı
+        bool withDeleted = false,
+        bool enableTracking = true,
+        CancellationToken cancellationToken = default);
+
+    IPaginate<TEntity> GetList(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true,
+        CancellationToken cancellationToken = default);
+
+    IPaginate<TEntity> GetListByDynamic(
+        DynamicQuery dynamic,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true,
+        CancellationToken cancellationToken = default);
+
+    // bizim aradığımız veri var mı yok mu diye bakar.
+    bool Any(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        bool withDeleted = false,
+        bool enableTracking = true,
+        CancellationToken cancellationToken = default);
+
+    TEntity Add(TEntity entity);
+    ICollection<TEntity> AddRange(ICollection<TEntity> entity);
+    TEntity Update(TEntity entity);
+    ICollection<TEntity> UpdateRange(ICollection<TEntity> entity);
+    TEntity Delete(TEntity entity, bool permanent = false);
+    ICollection<TEntity> DeleteRange(ICollection<TEntity> entity, bool permanent = false);
+
+}
+
